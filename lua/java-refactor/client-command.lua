c6189efa2f0b02030f611c8cf8bd8e4387568844
s1n7ax <srineshnisala@gmail.com>
2024-07-24 22:36:39 +0530

    organize code

--- lua/java-refactor/client-command.lua
+++ lua/java-refactor/client-command.lua
@@ -1,6 +1,5 @@
-local M = {}
-
-M.commands = {
+---@enum jdtls.ClientCommand
+local M = {
 	ADD_TO_SOURCEPATH = 'java.project.addToSourcePath',
 	ADD_TO_SOURCEPATH_CMD = 'java.project.addToSourcePath.command',
 	APPLY_REFACTORING_COMMAND = 'java.action.applyRefactoringCommand',
@@ -90,101 +89,5 @@ M.commands = {
 	UPGRADE_GRADLE_WRAPPER = 'java.project.upgradeGradle',
 	UPGRADE_GRADLE_WRAPPER_CMD = 'java.project.upgradeGradle.command',
 }
-
----@param message string
----@param func fun(action: java-refactor.Action)
-local run = function(message, func)
-	local runner = require('async.runner')
-	local get_error_handler = require('java-refactor.utils.error_handler')
-	local instance = require('java-refactor.utils.instance-factory')
-
-	runner(function()
-			func(instance.get_action())
-		end)
-		.catch(get_error_handler(message))
-		.run()
-end
-
-M.handlers = {
-	---@param params java-refactor.RenameAction[]
-	[M.commands.RENAME_COMMAND] = function(params)
-		run('Failed to rename the symbol', function(action)
-			action:rename(params)
-		end)
-	end,
-
-	---@param params nvim.CodeActionParamsResponse
-	[M.commands.GENERATE_CONSTRUCTORS_PROMPT] = function(_, params)
-		run('Failed to generate constructor', function(action)
-			action:generate_constructor(params)
-		end)
-	end,
-
-	---@param params nvim.CodeActionParamsResponse
-	[M.commands.GENERATE_TOSTRING_PROMPT] = function(_, params)
-		run('Failed to generate toString', function(action)
-			action:generate_to_string(params)
-		end)
-	end,
-
-	---@param params nvim.CodeActionParamsResponse
-	[M.commands.HASHCODE_EQUALS_PROMPT] = function(_, params)
-		run('Failed to generate hash code and equals', function(action)
-			action:generate_hash_code_and_equals(params)
-		end)
-	end,
-
-	---@param params nvim.CodeActionParamsResponse
-	[M.commands.GENERATE_DELEGATE_METHODS_PROMPT] = function(_, params)
-		run('Failed to generate delegate methods', function(action)
-			action:generate_delegate_mothods_prompt(params)
-		end)
-	end,
-
-	---@param command lsp.Command
-	[M.commands.APPLY_REFACTORING_COMMAND] = function(command)
-		run('Failed to apply refactoring command', function(action)
-			action:apply_refactoring_command(command)
-		end)
-	end,
-
-	---@param is_full_build boolean
-	[M.commands.COMPILE_WORKSPACE] = function(is_full_build)
-		run('Failed to build workspace', function(action)
-			action:build_workspace(is_full_build)
-		end)
-	end,
-
-	[M.commands.CLEAN_WORKSPACE] = function()
-		run('Failed to clean workspace', function(action)
-			action:clean_workspace()
-		end)
-	end,
-}
-
-local ignored_commands = { M.commands.REFRESH_BUNDLES_COMMAND }
-
-for _, command in pairs(M.commands) do
-	if
-		not M.handlers[command] and not vim.tbl_contains(ignored_commands, command)
-	then
-		local message = string.format(
-			'"%s" is not supported yet!'
-				.. '\nPlease request the feature using below link'
-				.. '\nhttps://github.com/nvim-java/nvim-java/issues/new?assignees='
-				.. '&labels=enhancement&projects=&template=feature_request.yml&title=feature%%3A+',
-			command
-		)
-
-		M.handlers[command] = function()
-			require('java-core.utils.notify').warn(message)
-
-			return vim.lsp.rpc_response_error(
-				vim.lsp.protocol.ErrorCodes.MethodNotFound,
-				'Not implemented yes'
-			)
-		end
-	end
-end
 
 return M
--- lua/java-refactor/init.lua
+++ lua/java-refactor/init.lua
@@ -1,25 +1,39 @@
 local event = require('java-core.utils.event')
 
+local M = {}
+
 local group = vim.api.nvim_create_augroup('java-refactor-command-register', {})
 
-local setup = function()
-	-- setting all the vim.lsp.commands
-	local code_action_handler = require('java-refactor.code-action-handlers')
+event.on_jdtls_attach({
+	group = group,
+	once = true,
+	callback = function()
+		M.reg_client_commands()
+		M.reg_refactor_commands()
+		M.reg_build_commands()
+	end,
+})
 
-	for key, handler in pairs(code_action_handler.handlers) do
+M.reg_client_commands = function()
+	local code_action_handlers = require('java-refactor.client-command-handlers')
+
+	for key, handler in pairs(code_action_handlers) do
 		vim.lsp.commands[key] = handler
 	end
+end
 
-	-- setting all the user commands and APIs
-	local code_action_api = require('java-refactor.api.code-action')
+M.reg_refactor_commands = function()
+	local code_action_api = require('java-refactor.api.refactor')
 
 	for api_name, api in pairs(code_action_api) do
 		require('java').register_api({ 'refactor', api_name }, api, { range = 2 })
 	end
 end
 
-event.on_jdtls_attach({
-	group = group,
-	once = true,
-	callback = setup,
-})
+M.reg_build_commands = function()
+	local code_action_api = require('java-refactor.api.build')
+
+	for api_name, api in pairs(code_action_api) do
+		require('java').register_api({ 'build', api_name }, api, {})
+	end
+end
